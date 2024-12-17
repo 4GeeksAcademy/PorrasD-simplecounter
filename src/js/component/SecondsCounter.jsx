0afb623b9b4efe5a6e36dd0e537722c98a00a127
PorrasD <122325175+PorrasD@users.noreply.github.com>
2024-12-17 20:56:32 +0000

    simple counter finished

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,90 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Spinner } from 'react-bootstrap';
-import { Button, Container, Row, Col, Card } from 'react-bootstrap';
-import { FaRedo } from 'react-icons/fa';
+import { BiAlarm } from "react-icons/bi";
+
 
 const SecondsCounter = () => {
-  const [count, setCount] = useState(0);
-  const [isActive, setIsActive] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [digits, setDigits] = useState([0, 0, 0, 0]);
 
   const toggleCounter = () => {
     setIsActive(prevState => !prevState);
   };
 
-  const resetCounter = () => {
-    setCount(0);
-    setIsActive(false);
-  };
-
   useEffect(() => {
-    let interval;
-    if (isActive) {
-      interval = setInterval(() => {
-        setCount(prevCount => prevCount + 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive]);
+    const interval= setInterval(()=>{
+      const digitsArray= seconds.toString().padStart(4, "0").split("");
+
+      setDigits(digitsArray);
+      setSeconds((prevSeconds)=> prevSeconds + 1);
+    },1000);
+
+    return() => clearInterval(interval);
+}, [seconds]);
 
   return (
-    <Container 
-      fluid 
-      style={{
-        minHeight: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Card style={{ width: '18rem', textAlign: 'center', padding: '20px' }}>
-        <Card.Body>
-          <Card.Title>Contador:</Card.Title>
-          <div className="counter-display" style={{ marginBottom: '20px' }}>
-            {isActive && (
-              <Spinner 
-                animation="border" 
-                role="status" 
-                style={{ fontSize: '50px', marginBottom: '10px' }}
-              >
-                <span className="visually-hidden">Cargando...</span>
-              </Spinner>
-            )}
-            <h1 
-              style={{ 
-                color: isActive ? 'black' : 'gray', 
-                fontFamily: "fantasy", 
-                fontSize: '3rem',
-                marginTop: '10px'
-              }}
-            >
-              {count}
-            </h1>
-          </div>
-          <Row>
-            <Col className="d-flex justify-content-center" style={{ marginBottom: '10px' }}>
-              <Button 
-                variant="dark" 
-                onClick={toggleCounter} 
-                style={{ width: '120px', fontSize: '1.2rem' }}
-              >
-                {isActive ? 'Pause' : 'Start'}
-              </Button>
-            </Col>
-            <Col className="d-flex justify-content-center">
-              <Button 
-                variant="primary" 
-                onClick={resetCounter} 
-                style={{ width: '120px' }}
-              >
-              <FaRedo />
-              </Button>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    </Container>
+    
+    <div className='container row d-flex bg-black d-grid vh-100 m-auto'>
+      <div className='display-1 text-warning mt-5'><BiAlarm />      </div>
+
+      <div className='col' ><p className='fs-1 bg-white'>{digits[0]}</p></div>
+      <div className='col'><p className='fs-1 bg-white'>{digits[1]}</p></div>
+      <div className='col'><p className='fs-1 bg-white'>{digits[2]}</p></div>
+      <div className='col'><p className='fs-1 bg-white'>{digits[3]}</p></div>
+      
+    </div>
   );
 };
 
